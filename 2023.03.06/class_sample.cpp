ee1d2a6e72fb7cfa752a4aaa25975b4067cf1078
miyalab <miyalab.workspace@gmail.com>
2023-03-08 16:50:22 +0900

    update

---
+++ 2023.03.06/class_sample.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Human{
+public:
+    Human();                // コンストラクタ
+    Human(const int &_age); // コンストラクタのオーバーロード
+    virtual ~Human();       // デストラクタ
+    
+    void setAge(const int &_age);
+    const int &getAge() const;
+
+    void say();
+
+private:
+    int age;
+protected:
+
+};
+
+int main()
+{
+    Human human1;
+    Human human2();
+    Human human3(23);
+
+    std::cout << human1.getAge() << std::endl;
+    std::cout << human2.getAge() << std::endl;
+    std::cout << human3.getAge() << std::endl;
+}
+
+Human::Human()
+{
+
+}
+
+Human::Human(const int &_age)
+{
+    this->setAge(_age);
+}
+
+Human::~Human()
+{
+
+}
+
+void Human::setAge(const int &_age)
+{
+    this->age = _age;
+}
+
+const int &Human::getAge() const
+{
+    return age;
+}
