4f87c375f9758f0eaf62f60ec5d3a6ac9db5d9e8
miyalab <miyalab.workspace@gmail.com>
2023-04-18 17:50:20 +0900

    update

--- 2023.04.18/tutorial_parameter/src/parameter.cpp
+++ 2023.04.18/tutorial_parameter/src/parameter.cpp
@@ -11,8 +11,9 @@ int main(int argc, char **argv)
     auto num = node->declare_parameter<double>("double_val", 0.0);
 
     for(rclcpp::WallRate loop(1); rclcpp::ok(); loop.sleep()){
-        RCLCPP_INFO(node->get_logger(), "val: %d", val);
+        RCLCPP_INFO(node->get_logger(), "val: %ld", val);
         RCLCPP_INFO(node->get_logger(), "str: %s", str.c_str());
         RCLCPP_INFO(node->get_logger(), "num: %lf", num);
     }
+    rclcpp::shutdown();
 }
