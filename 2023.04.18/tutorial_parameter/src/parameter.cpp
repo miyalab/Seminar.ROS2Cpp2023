cf9313d6ebd43127de84648ae934b65f386e2c4c
miyalab <miyalab.workspace@gmail.com>
2023-04-18 17:19:04 +0900

    update

---
+++ 2023.04.18/tutorial_parameter/src/parameter.cpp
@@ -0,0 +1,18 @@
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("parameter");
+
+    // パラメータ読み込み
+    auto val = node->declare_parameter<int>("int_val", 0);
+    auto str = node->declare_parameter<std::string>("string", "hello");
+    auto num = node->declare_parameter<double>("double_val", 0.0);
+
+    for(rclcpp::WallRate loop(1); rclcpp::ok(); loop.sleep()){
+        RCLCPP_INFO(node->get_logger(), "val: %d", val);
+        RCLCPP_INFO(node->get_logger(), "str: %s", str.c_str());
+        RCLCPP_INFO(node->get_logger(), "num: %lf", num);
+    }
+}
